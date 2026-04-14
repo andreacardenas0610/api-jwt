dda8aa470651a0b54bdf3a8eef8e9df549f7cb53
andreacardenas0610 <andrea.cardenas0610@gmail.com>
2026-04-13 21:41:46 -0500

    api

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.use(express.json());
 // Cargar rutas
 app.use('/api/items', require('./routes/items'));
 
-const PORT = 4001; 
+// --- EL CAMBIO IMPORTANTE AQUÍ ---
+// process.env.PORT lee el puerto que le asigne Railway
+// Si no existe (como en tu PC), usa el 4001 por defecto
+const PORT = process.env.PORT || 4001; 
+
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor listo en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor listo en el puerto ${PORT}`);
 });
